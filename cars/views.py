7b07742136d58da75930b25400e6e55a7a470894
Andrew Witmer <witmer.a@gmail.com>
2023-03-24 16:22:10 -0700

    Completed DELETE Functionality for Car View File

--- cars/views.py
+++ cars/views.py
@@ -24,7 +24,7 @@ def cars_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def car_detail(request, pk):
     
     car = get_object_or_404(Car, pk=pk)
@@ -37,4 +37,7 @@ def car_detail(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        car.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
